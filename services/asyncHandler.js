9f844758f46cbda5fbde1ca0d03ce81125d93dc6
mehulsjain <mehulsjain1@gmail.com>
2023-01-21 23:01:47 +0530

    Design meathods for asyncHandler and error

---
+++ services/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export default asyncHandler;
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+
+//     }catch(err){
+    
+//     }
+// }
+
+// this is equivalent to 
+
+// function asyncHandler(fn){
+//     return async function(req, res, next){
+//         try{
+
+//         }catch(err){
+
+//         }
+//     }
+// }
